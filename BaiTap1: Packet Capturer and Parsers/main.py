e17e93e606e14c15f20bacaf39ba0ca04bee201f
QuocGia12 <nguyengia595@gmail.com>
2026-09-21 14:48:44 +0700

    implement CLI

---
+++ BaiTap1: Packet Capturer and Parsers/main.py
@@ -0,0 +1,36 @@
+import argparse 
+
+
+'''
+Các options cho CLI là: 
+- --interface  
+- --pcap 
+- --output, default is result.json
+Chỉ được sử dụng chính xác một trong 2 options đó 
+'''
+def build_CLI() -> argparse.ArgumentParser: 
+    parser = argparse.ArgumentParser(
+        description='The program for capturing and parsering packets, use --interface for live capturing or use --pcap for importing pcap file'
+    )
+    source = parser.add_mutually_exclusive_group(required=True)
+    source.add_argument(
+        "--interface", help="Identify network interface for live capturing"
+    )
+    source.add_argument(
+        "--pcap", help="Identify pcap file"
+    )
+    parser.add_argument(
+        "--output", 
+        default="result.json",
+        help="Identify output file, the default is result.json"
+    )
+    return parser 
+
+def main(): 
+    CLI = build_CLI()
+    args = CLI.parse_args()
+    print(args.interface, args.pcap, args.output)
+
+
+if __name__ == "__main__": 
+    main()
